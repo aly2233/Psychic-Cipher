00f7b44547b0e1ba3841a9490dd2425eeec0ae0a
Zachary Werbalowsky <zachwerbalowsky@Zacharys-MacBook-Pro.local>
2022-04-04 11:27:15 -0700

    session back login and signup working through postman

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
 const express = require("express");
+const app = express();
 const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
+const bodyParser = require('body-parser');
+const passport = require('passport');
+
+const users = require("./routes/api/users");
 
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
-  const app = express();
 
 app.get("/", (req, res) => res.send("Hello World"));
+
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/api/users", users);
+
+
+
+
+
 
 const port = process.env.PORT || 4000;
 
