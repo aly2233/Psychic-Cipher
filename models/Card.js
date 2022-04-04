45478bf0495724684c0fa3d3539525a996e9c5d0
ajoyvmon <ajoyvmon@gmail.com>
2022-04-04 11:34:09 -0700

    card model

---
+++ models/Card.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const CardSchema = new Schema({
+  arcana: {
+    type: String,
+    required: true
+  },
+  nameShort: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  value: {
+    type: String,
+    required: true
+  },
+  valueInt: {
+    type: Number,
+    required: true
+  },
+  meaningUp: {
+    type: String,
+    required: true
+  },
+  meaningRev: {
+    type: String,
+    required: true
+  },
+  desc: {
+    type: Text,
+    required: true
+  }
+}, {
+  timestamps: true
+});
+
+module.exports = Card = mongoose.model("Card", CardSchema)
