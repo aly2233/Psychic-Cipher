30016e84c95c5c3ef7eb59e487f4b098d5410c56
ajoyvmon <ajoyvmon@gmail.com>
2022-04-05 19:10:20 -0700

    add photo urls up to justice

--- frontend/src/components/cards/reading.jsx
+++ frontend/src/components/cards/reading.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
 class Reading extends React.Component {
-  // componentDidMount(){
-  //   this.props.fetchCards();
-  // }
+  componentDidMount(){
+    this.props.fetchCards();
+  }
 
   render(){
     console.log(this.props);
