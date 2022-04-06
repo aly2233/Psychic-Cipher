ac73df0ca011800ed692e2f3d05791fdf793ef80
ajoyvmon <ajoyvmon@gmail.com>
2022-04-06 11:43:48 -0700

    restore readings

--- frontend/src/components/cards/reading.jsx
+++ frontend/src/components/cards/reading.jsx
@@ -1,17 +1,49 @@
-// import React from "react";
+import React from "react";
 
-// class Reading extends React.Component {
-//   componentDidMount(){
-//     this.props.fetchCards();
-//   }
+class Reading extends React.Component {
+  componentDidMount() {
+    this.props.fetchCards();
+  }
 
-//   render(){
-//     console.log(this.props);
-//     // this.props.fetchCards();
-//     return (
-//       <div>I am from the reading</div>
-//     )
-//   }
-// }
+  render() {
+    const { cards } = this.props;
+    // console.log(cards)
 
-// export default Reading;
+    function cardReading(reqLength) {
+      const handIdx = [];
+      const handArr = [];
+
+      // // saving random indexes into array
+      while (handIdx.length < reqLength) {
+        const randNum = Math.floor(Math.random() * 78);
+        if (!handIdx.includes(randNum)) handIdx.push(randNum);
+      }
+
+      console.log("These are random indexes: ", handIdx);
+
+      // // iterating through array of random indexes
+      for (let i = 0; i < handIdx.length; i++) {
+        const randPos = Math.floor(Math.random() * 2);
+        // console.log(cards)
+        console.log(typeof handIdx[i]);
+        // console.log(cards[handIdx[i]]);
+        // const finalCard = cards[handIdx[i]];
+        console.log(cards[9]);
+        const finalPos = randPos === 0 ? "meaningUp" : "meaningRev"
+
+        // console.log("This is the Final Card: ", finalCard);
+        console.log("Final Position: ", finalPos);
+      }
+
+      // return hand;
+    }
+
+    console.log(cardReading(5))
+
+    return (
+      <div>I am from the reading</div>
+    )
+  }
+}
+
+export default Reading;
