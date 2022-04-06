5cfce0ea72b334735e8643dbf521262b9286c734
ajoyvmon <ajoyvmon@gmail.com>
2022-04-05 18:07:36 -0700

    create reading react component

---
+++ frontend/src/components/cards/reading.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+class Reading extends React.Component {
+  // componentDidMount(){
+  //   this.props.fetchCards();
+  // }
+
+  render(){
+    console.log(this.props);
+    // this.props.fetchCards();
+    return (
+      <div>I am from the reading</div>
+    )
+  }
+}
+
+export default Reading;
