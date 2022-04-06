5f2304a753f6ada6801c48813268ca1e462c6736
David Domingo <ddoming824@gmail.com>
2022-04-06 11:23:17 -0700

    stuff

--- frontend/src/components/posts/post_index.jsx
+++ frontend/src/components/posts/post_index.jsx
@@ -1,30 +1,35 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import PostIndexItemContainer from './post_index_item_container';
 import postStyles from './posts.css'
-import {Link} from 'react-router-dom'
+import {Link, Route} from 'react-router-dom'
+import CreatePostFormContainer from './post_form/create_post_container';
 
-const PostIndex = ({card = {id: '624c87b71b90dbf9947ba2fe'}, posts, fetchPosts}) => {
 
-    //test//
-    card = {id: '624c87b71b90dbf9947ba2fe'}
-    //////
+const PostIndex = ({card, posts, fetchPosts}) => {
+
+    const [createPost, setCreatePost] = useState(false)
+    
 
     useEffect( () => {
-        fetchPosts('624c87b71b90dbf9947ba2fe');
+        fetchPosts(card._id);
     },[])
 
-
-    // if(!posts) {
-    //     return <></>
-    // }
+    const toggleCreatePostWindow = () => {
+        createPost ? setCreatePost(false) : setCreatePost(true)
+    }
 
     return(
-        <ul className='posts-list'>
-            {posts?.map( post => {
-                return <PostIndexItemContainer key={post.id} post={post}/>
-            })}
-            <Link to="/posts/new" className='link-button'>Create New Post</Link>
-        </ul>
+        <div>
+            <ul className='posts-list'>
+                {posts?.map( post => {
+                    return <PostIndexItemContainer key={post.id} post={post}/>
+                })}
+                {/* <Link to={`cards/${card._id}/new`} className='link-button'>Create New Post</Link> */}
+            </ul>
+            {/* <Route exact path="/posts/new" component={CreatePostFormContainer}/> */}
+            <button onClick={toggleCreatePostWindow} className='link-button'>Create New Post</button>
+            {createPost ? <CreatePostFormContainer /> : <></>}
+        </div>
     )
 }
 
