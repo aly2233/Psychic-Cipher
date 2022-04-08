fa8d88e743d67eac67eba29cfb0b76151d73dc9f
ajoyvmon <ajoyvmon@gmail.com>
2022-04-07 21:10:42 -0700

    profile css make cohesive

--- frontend/src/components/profile/profile.jsx
+++ frontend/src/components/profile/profile.jsx
@@ -1,36 +1,58 @@
 import React from "react";
-import { connect } from "react-redux";
-import PostIndexContainer from "../posts/post_index_container"
+import PostIndexContainer from "../posts/post_index_container";
+import "./profile.css";
 
-const Profile = ({props}) => {
-  console.log( props );
+const Profile = () => {
   return (
     <div id="profile-container">
-      <div>
+      <div id="authored-posts">
         <h1>Tarot Notes</h1>
         <PostIndexContainer />        
       </div>
 
-      <div>
-        <h1>Friends</h1>
-        <h2>David Domingo</h2>
-        <a href="https://github.com/Domingo-creator">Github</a>
-        <h2>Abigail Montemayor</h2>
-        <a href="https://github.com/ee3y0re">Github</a>
-        <h2>Zach Werbalowsky</h2>
-        <a href="https://github.com/ZWerbo">Github</a>
-        <h2>Alan Yueh</h2>
-        <a href="https://github.com/aly2233">Github</a>
+        <div id="friends-container">
+          <h1>Keep in touch with your friends!</h1>
+          <div id="friend-boundaries">
+            <div id="friend-box-top" className="friend">
+              <div className="friend-heading">
+                <h2>David Domingo</h2>
+              </div>
+              <div className="friend-link">
+              <a href="https://github.com/Domingo-creator">Domingo-creator</a>
+              </div>
+            </div>
+
+          <div id="space-between-friend" className="friend">
+            <div className="friend-heading">
+              <h2>Zach Werbalowsky</h2>
+            </div>
+            <div className="friend-link">
+              <a href="https://github.com/ZWerbo">ZWerbo</a>
+            </div>
+          </div>
+          
+          <div id="space-between-friend" className="friend">
+            <div className="friend-heading">
+              <h2>Alan Yueh</h2>
+            </div>
+            <div className="friend-link">
+              <a href="https://github.com/aly2233">aly2233</a>          
+            </div>
+          </div>
+
+          <div id="space-between-friend" className="friend">
+            <div className="friend-heading">
+              <h2>Abigail Montemayor</h2>
+            </div>
+            <div className="friend-link">
+              <a href="https://github.com/ee3y0re">ee3y0re</a>
+            </div>
+          </div>
+        </div>
       </div>
       
     </div>
   );
 }
 
-const mSTP = (state) => {
-  return {
-    props: state.session.user
-  }
-}
-
-export default connect(mSTP)(Profile);
+export default Profile;
