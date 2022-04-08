86249b631364a1ca4d20830ed101e4fdad502eb6
ajoyvmon <ajoyvmon@gmail.com>
2022-04-07 17:23:18 -0700

    filled out developer profiles

--- frontend/src/components/profile/profile.jsx
+++ frontend/src/components/profile/profile.jsx
@@ -5,8 +5,24 @@ import PostIndexContainer from "../posts/post_index_container"
 const Profile = ({props}) => {
   console.log( props );
   return (
-    <div>
-      <PostIndexContainer />
+    <div id="profile-container">
+      <div>
+        <h1>Tarot Notes</h1>
+        <PostIndexContainer />        
+      </div>
+
+      <div>
+        <h1>Friends</h1>
+        <h2>David Domingo</h2>
+        <a href="https://github.com/Domingo-creator">Github</a>
+        <h2>Abigail Montemayor</h2>
+        <a href="https://github.com/ee3y0re">Github</a>
+        <h2>Zach Werbalowsky</h2>
+        <a href="https://github.com/ZWerbo">Github</a>
+        <h2>Alan Yueh</h2>
+        <a href="https://github.com/aly2233">Github</a>
+      </div>
+      
     </div>
   );
 }
