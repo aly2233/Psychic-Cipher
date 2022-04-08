b894f27d051314061d5ecb4a639bfb2a61be44bf
David Domingo <ddoming824@gmail.com>
2022-04-07 21:31:20 -0700

    fixed post handles on front page

--- frontend/src/components/posts/frontpage/post_frontpage_container.js
+++ frontend/src/components/posts/frontpage/post_frontpage_container.js
@@ -2,19 +2,23 @@ import { connect } from "react-redux"
 import { withRouter } from "react-router-dom"
 // import {fetchPosts } from "../../actions/post_actions"
 import { fetchPosts } from "../../../actions/post_actions"
+import { fetchUser } from "../../../actions/user_actions"
 import PostFrontpage from "./post_frontpage.jsx"
 
 
 const mapStateToProps = (state, ownProps) => {
     return {
         posts: Object.values(state.posts),
-        loggedIn: state.session.isAuthenticated
+        loggedIn: state.session.isAuthenticated,
+        authors: state.users
+
     }
 }
 
 const mapDispatchToProps = dispatch => {
     return {
-        fetchPosts: (limit, skip) => dispatch(fetchPosts(null, null, limit, skip))
+        fetchPosts: (limit, skip) => dispatch(fetchPosts(null, null, limit, skip)),
+        fetchAuthor: (userId) => dispatch(fetchUser(userId))
     }
 }
 
--- frontend/src/components/posts/frontpage/post_frontpage_item.jsx
+++ frontend/src/components/posts/frontpage/post_frontpage_item.jsx
@@ -1,17 +1,17 @@
 import React, {useEffect, useState} from 'react'
 
 
-const PostFrontpageItem = ({post, user}) => {
+const PostFrontpageItem = ({post, author}) => {
 
     const convertDate = () => {
         let dateString = new Date(post.date).toString()
         return dateString.slice(0, dateString.indexOf(':') - 2)
     }
-
+    
     return (
         <li>
             <div className='post-header'>
-                <p className='user-email-tag'>{user?.email}</p>
+                <p className='user-email-tag'>{author?.data?.handle}</p>
                 <p className='date-tag'>{convertDate()}</p>
                 {/* <p> {post.date}</p> */}
             </div>
