972651d184990073422a91869a0ebd1f8b00e6ea
ajoyvmon <ajoyvmon@gmail.com>
2022-04-18 15:09:41 -0700

    start basic skeleton footer

---
+++ frontend/src/components/about_footer/about_footer.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const AboutFooter = () => {
+  return (
+    <div id="about-container">
+      <ul id="dev-links">
+        <li className="dev-individ">
+          <p className="dev-name">Alan Yueh</p>
+          <div className="dev-links-contain">
+            <a href=""></a>
+          </div>
+        </li>
+        <li className="dev-individ">
+          <p className="dev-name">Zach Werbalowsky</p>
+          <div className="dev-links-contain">
+            <a href=""></a>
+          </div>
+        </li>
+        <li className="dev-individ">
+          <p className="dev-name">Abigail Montemayor</p>
+          <div className="dev-links-contain">
+            <a href=""></a>
+          </div>
+        </li>
+        <li className="dev-individ">
+          <p className="dev-name">David Domingo</p>
+          <div className="dev-links-contain">
+            <a href=""></a>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default AboutFooter
